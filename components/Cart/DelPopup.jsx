f8a3d02d9e2dad046c8bd1c1105487b5c1afb84a
Quang Promise <you@example.com>
2023-12-08 20:08:54 +0700

    update del popup

--- components/Cart/DelPopup.jsx
+++ components/Cart/DelPopup.jsx
@@ -17,7 +17,7 @@ function DelPopup({ isClose }) {
             
             <div className="bg-black w-full h-full z-20 fixed opacity-50">
         </div>
-            <div className="z-30 max-lg:left-20 max-lg:w-1/3 max-md:w-1/2 max-md:-left-36 fixed w-2/5 top-32 flex-col left-40  ml-64 p-10 bg-white rounded border-2 border-gray-500">
+            <div className="z-30  max-lg:left-20 max-lg:w-2/3 max-md:w-2/3 max-md:-left-44 fixed w-2/5 top-32 flex-col left-40  ml-64 p-10 bg-white rounded border-2 border-gray-500">
                 <h1 className="mb-8 text-black font-bold text-center">Are you sure to delete this item?</h1>
                 <div className="flex mx-auto justify-around">
                     <button onClick={isClose} className="hover:opacity-50">Close</button>
